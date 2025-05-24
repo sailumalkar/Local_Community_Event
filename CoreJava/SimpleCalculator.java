e343ec4d57e2fa2eeff7b3d9f5dfabd8c37be9a8
sailumalkar <sailulakshmi2005@gmail.com>
2025-05-24 23:24:09 +0530

    Create SimpleCalculator.java

---
+++ CoreJava/SimpleCalculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+        System.out.print("Choose operation (+, -, *, /): ");
+        char op = scanner.next().charAt(0);
+        double result;
+        switch (op) {
+            case '+': result = num1 + num2; break;
+            case '-': result = num1 - num2; break;
+            case '*': result = num1 * num2; break;
+            case '/': result = num2 != 0 ? num1 / num2 : Double.NaN; break;
+            default: result = Double.NaN;
+        }
+        System.out.println("Result: " + result);
+    }
+}
