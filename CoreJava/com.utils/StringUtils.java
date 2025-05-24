2c4505f79651fc842bc41d1f2fe0b3a3294e82d0
sailumalkar <sailulakshmi2005@gmail.com>
2025-05-24 23:56:19 +0530

    Create StringUtils.java

---
+++ CoreJava/com.utils/StringUtils.java
@@ -0,0 +1,10 @@
+package com.utils;
+
+public class StringUtils {
+    public static String capitalize(String text) {
+        if (text == null || text.isEmpty()) {
+            return text;
+        }
+        return Character.toUpperCase(text.charAt(0)) + text.substring(1);
+    }
+}
