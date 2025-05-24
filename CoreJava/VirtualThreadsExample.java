7f65ea0dc7e9e212bc775560a968a6695c485bdc
sailumalkar <sailulakshmi2005@gmail.com>
2025-05-25 00:05:40 +0530

    Create VirtualThreadsExample.java

---
+++ CoreJava/VirtualThreadsExample.java
@@ -0,0 +1,7 @@
+public class VirtualThreadsExample {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 100_000; i++) {
+            Thread.startVirtualThread(() -> System.out.println("Hello from virtual thread!"));
+        }
+    }
+}
